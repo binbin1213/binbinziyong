22150917503f444853abc5f461b0995c7b2492ca
彬彬哦 <32067712+binbin1213@users.noreply.github.com>
2021-09-21 14:18:40 +0800

    Update QQreadCOOKIE.js

--- QQreadCOOKIE.js
+++ QQreadCOOKIE.js
@@ -1,10 +1,10 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let QQreadurlVal = ``
+let QQreadurlVal = `book_sid=4c82bd7cbdbb4206b2bed6d7e8900ba8`
 
 
 
 
-let QQreadurlcookie = {book_sid=4c82bd7cbdbb4206b2bed6d7e8900ba8
+let QQreadurlcookie = {
 
     QQreadurlVal: QQreadurlVal,
 
