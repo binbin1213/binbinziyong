933df6554898b7330d78bddfbbb78ae708c37963
彬彬哦 <32067712+binbin1213@users.noreply.github.com>
2021-09-21 14:16:28 +0800

    Create QQreadCOOKIE.js

---
+++ QQreadCOOKIE.js
@@ -0,0 +1,13 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let QQreadurlVal = ``
+
+
+
+
+let QQreadurlcookie = {book_sid=4c82bd7cbdbb4206b2bed6d7e8900ba8
+
+    QQreadurlVal: QQreadurlVal,
+
+}
+
+module.exports = QQreadurlcookie
