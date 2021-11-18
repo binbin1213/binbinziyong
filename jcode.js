0c5d23d6d6b2d162e4d90ff5a483afbb824647dc
彬彬哦 <32067712+binbin1213@users.noreply.github.com>
2021-11-18 17:34:10 +0800

    Add files via upload

---
+++ jcode.js
@@ -0,0 +1,35 @@
+// 京东口令
+// [rule: raw [\s\S]*[#|@|$|%]([0-9a-zA-Z]{14})[#|@|$|%][\s\S]*]
+var jcode = param(1);
+
+function main() {
+    var exist = bucketGet("jcode", jcode)
+    if (exist) {
+        sendText(exist)
+        return
+    }
+    for (var i = 0; i < 10; i++) {
+        var data = request({
+            method: "get",
+            url: "https://service-crgecqni-1251133039.gz.apigw.tencentcs.com/release/code?code=(" + jcode + ")",
+            dataType: "json",
+        });
+        if (data) {
+            var replyMsg = "请检查你的口令是否正确。";
+            if (data.code == 0) {
+                replyMsg =
+                    "活动标题：" +
+                    data.data.title +
+                    "\n用户昵称：" +
+                    data.data.userName[0] +
+                    "****\n活动链接：" +
+                    data.data.jumpUrl;
+            }
+            sendText(replyMsg)
+            bucketSet("jcode", jcode, replyMsg)
+            break
+        }
+    }
+}
+
+main()
